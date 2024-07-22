7725db494ab3388e3384d3d2eabe4ab3624f9bbd
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-07-22 16:23:22 -0500

    Add some code resolution

---
+++ Puzzle/Easy/brackets-extreme-edition.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class S
+{
+    static void Main(string[] args)
+    {
+        string line = Console.ReadLine();
+        Dictionary<char, int> elements = new Dictionary<char, int>();
+        string init = "([{)]}";
+        bool contain = false;
+        
+        foreach(char c in init.ToCharArray())
+        {
+            elements.Add(c,0);
+        }
+        
+        foreach(char c in line.ToCharArray())
+        {
+            if(c == '(' || c == '[' || c == '{')
+            {
+                elements[c] += 1;
+            }
+            if(c == ')' || c == ']' || c == '}')
+            {
+                elements[c] += 1;
+            }
+        }
+
+        int ch1 = elements['(']-elements[')'];
+        int ch2 = elements['[']-elements[']'];
+        int ch3 = elements['{']-elements['}'];
+
+        foreach(char c in init.ToCharArray())
+        {
+            if(init.Contains(c))
+                contain = true;
+        }
+        
+        if((line.FirstOrDefault() == ']' && line.LastOrDefault() == '[') || line == "{(})" || (line.FirstOrDefault() == ')' && line.LastOrDefault() == '('))
+            contain = false;
+
+        if(ch1 == 0 && ch2 == 0 && ch3 == 0 && contain)
+            Console.WriteLine("true");
+        else
+            Console.WriteLine("false");
+    }
+}
---
+++ Puzzle/Easy/encryptiondecryption-of-enigma-machine.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Solution
+{
+    public const string dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static void Main(string[] args)
+    {
+        string operation = Console.ReadLine();
+        int RandomNumber = int.Parse(Console.ReadLine());
+        List<string> rotors = new List<string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            string rotor = Console.ReadLine();
+            rotors.Add(rotor);
+        }
+        
+        string message = Console.ReadLine();
+        string R = "";
+
+        if(operation == "ENCODE")
+        {
+            R = First(message, RandomNumber);
+            R = EncodeRotor(R, rotors[0]);
+            R = EncodeRotor(R, rotors[1]);
+            R = EncodeRotor(R, rotors[2]);
+        }
+        else
+        {
+            R = DecodeRotor(message, rotors[2]);
+            R = DecodeRotor(R, rotors[1]);
+            R = DecodeRotor(R, rotors[0]);
+            R = DecodeLast(R, RandomNumber);
+        }
+
+        Console.WriteLine(R);
+    }
+
+    static string First(string input, int RandomNumber)
+    {
+        string result = "";
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            int index = dic.IndexOf(input[i]) + RandomNumber + i;
+            if(index >= 26)
+                index = IndPoss(index);
+            result += dic.Substring(index,1);
+        }
+
+        return result;
+    }
+
+    static string EncodeRotor(string input, string rotor)
+    {
+        string result = "";
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            int index = dic.IndexOf(input[i]);
+            result += rotor.Substring(index,1);
+        }
+
+        return result;
+    }
+
+    static string DecodeRotor(string input, string rotor)
+    {
+        string result = "";
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            int index = rotor.IndexOf(input[i]);
+            result += dic.Substring(index,1);
+        }
+
+        return result;
+    }
+
+    static string DecodeLast(string input, int RandomNumber)
+    {
+        string result = "";
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            int index = dic.IndexOf(input[i]) - RandomNumber - i;
+            if(index < 0)            
+                index = IndNeg(index);
+            if(index >= 26)
+                index = IndPoss(index);
+
+            result += dic.Substring(index,1);
+        }
+
+        return result;
+    }
+
+    static int IndPoss(int val)
+    {
+        if(val >= 26)
+        {
+            val = val - 26;
+
+            if(val >= 26)
+             val = IndPoss(val);
+        }
+
+        return val;
+    }
+
+    static int IndNeg(int val)
+    {
+        if(val < 0)
+        {
+            val = 26 + val;
+
+            if(val < 0)
+                val = IndNeg(val);
+        }
+
+        return val;
+    }
+}
