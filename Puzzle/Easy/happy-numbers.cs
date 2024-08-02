34dcb7229c03007a4c5657eb3b117b88de8b7d3f
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-08-02 08:24:02 -0500

    Add some project

---
+++ Puzzle/Easy/happy-numbers.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            string x = Console.ReadLine();
+            if(IsHappy(BigInteger.Parse(x)))
+                Console.WriteLine($"{x} :)");
+            else
+                Console.WriteLine($"{x} :(");
+        }
+    }
+
+    static bool IsHappy(BigInteger number)
+    {
+        HashSet<BigInteger> seenNumbers = new HashSet<BigInteger>();
+
+        while (number != 1 && !seenNumbers.Contains(number))
+        {
+            seenNumbers.Add(number);
+            number = SumOfSquaresOfDigits(number);
+        }
+
+        return number == 1;
+    }
+
+    static BigInteger SumOfSquaresOfDigits(BigInteger number)
+    {
+        BigInteger sum = 0;
+
+        while (number > 0)
+        {
+            BigInteger digit = number % 10;
+            sum += digit * digit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
