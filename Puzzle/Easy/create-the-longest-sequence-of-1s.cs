9d42a7f7cc4ca405fca12aa97843ef1fdbd9f7e4
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-08-19 15:28:03 -0500

    Add some code resolution

---
+++ Puzzle/Easy/create-the-longest-sequence-of-1s.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Text;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int maxCount = 0;
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            if(input[i] == '1')
+                continue;
+            else
+            {
+                StringBuilder newInput = new StringBuilder(input);
+                newInput[i] = '1'; 
+
+                int count = FindMaxConsecutiveOnes(newInput.ToString());
+                if(count > maxCount)
+                    maxCount = count;
+            }
+        }
+
+        Console.WriteLine(maxCount);
+    }
+
+    public static int FindMaxConsecutiveOnes(string nums)
+    {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        foreach (char num in nums)
+        {
+            if (num == '1')
+            {
+                currentCount++;
+                maxCount = Math.Max(maxCount, currentCount);
+            }
+            else
+            {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
