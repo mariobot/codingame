447827e1a47d8f918138b7f780199ffc64ba122f
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-09-09 15:12:19 -0500

    Add some code resolution

---
+++ Puzzle/Easy/benfords-law.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        var list = new List<(string transaction, int value)>();
+
+        for (int i = 0; i < N; i++)
+        {
+            string transaction = Console.ReadLine();
+            int value = transaction.FirstOrDefault(c => c != '0' && char.IsDigit(c)) - '0';
+            if (value > 0)
+            {
+                list.Add((transaction, value));
+            }
+        }
+
+       var percentages = new Dictionary<int, double>
+        {
+            { 1, 30.1 },
+            { 2, 17.6 },
+            { 3, 12.5 },
+            { 4, 9.7 },
+            { 5, 7.9 },
+            { 6, 6.7 },
+            { 7, 5.8 },
+            { 8, 5.1 },
+            { 9, 4.6 }
+        };      
+        
+        double total = list.Count;
+        int state = percentages.Keys.Count(i =>
+        {
+            double countV = list.Count(x => x.value == i);
+            double p = countV / total * 100;
+            return p < percentages[i] - 10 || p > percentages[i] + 10;
+        });
+
+        Console.WriteLine(state > 0 ? "true" : "false");
+    }
+}
