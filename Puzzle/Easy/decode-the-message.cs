3bc181f8c628b6e0dc68dd2f6e6068d572bd51c7
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-09-23 16:07:55 -0500

    Add some code resolution

---
+++ Puzzle/Easy/decode-the-message.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        long P = long.Parse(Console.ReadLine());
+        int length = 0;
+        long Pt = P;
+        string C = Console.ReadLine();
+        List<char> list = new List<char>();
+        bool isFirst = true;
+        length = C.Length;
+
+        while (Pt > 0)
+        {
+            long remainder = Pt % length;
+            if(isFirst)
+            {
+                list.Add(C[(int)remainder]);
+                isFirst = false;
+            }
+            else
+            {
+                if (((int)remainder-1) == -1){
+                    Pt -= length;
+                }   
+                else
+                    list.Add(C[(int)remainder-1]);
+            }
+
+            Pt /= length;
+        }
+
+        Console.WriteLine(string.Join("",list));
+    }
+}
