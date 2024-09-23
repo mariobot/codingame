9fdd86f6efb03cccfaf67ee692af2e376b242834
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-09-23 16:08:09 -0500

    Add some code resolution

---
+++ Puzzle/Easy/fix-the-networks.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int m = int.Parse(Console.ReadLine());
+        for (int i = 0; i < m; i++)
+        {
+            string[] r = Console.ReadLine().Split("/");
+            string[] ip = r[0].Split(".");
+            int mask = int.Parse(r[1]);
+            int totalZeros, totalMask;
+
+            string binnaryIp = "";
+            
+            foreach(string val in ip)
+                binnaryIp += Convert.ToString(int.Parse(val), 2).PadLeft(8, '0');
+
+            var lastIndex = binnaryIp.LastIndexOf('1');
+
+            totalZeros = 31 - lastIndex;
+            totalMask = 32 - mask;
+
+            if(binnaryIp.Count(x => x =='0') == 32)
+            {
+                totalZeros = 32;
+                totalMask = 32 - mask;
+            }
+
+            if(totalZeros >= totalMask)
+            {
+                long totalConvinations = (long)Math.Pow((double)2, (double)totalMask);
+                Console.WriteLine($"valid {totalConvinations}");
+            }
+            else
+            {
+                int newMask = 32 - totalZeros;
+                long totalConvinations = (long)Math.Pow((double)2, (double)totalZeros);
+                Console.WriteLine($"invalid {string.Join(".",ip)}/{newMask} {totalConvinations}");
+            }
+        }
+    }
+}
