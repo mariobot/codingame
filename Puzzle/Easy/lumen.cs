9d2ea85405843ba9bc1c7e2fe5b14d4eff7ff444
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-10-11 19:01:52 -0500

    Add some code resolution

---
+++ Puzzle/Easy/lumen.cs
@@ -0,0 +1,72 @@
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int L = int.Parse(Console.ReadLine());
+        char[][] table = new char[N][];        
+        
+        for (int i = 0; i < N; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            char[] chars = new char[N];
+            for (int j = 0; j < N; j++)
+            {
+                string cell = inputs[j];
+                chars[j] = Convert.ToChar(cell);
+            }
+            table[i] = chars;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if(table[i][j] == 'C')
+                {
+                    int iCoord = i;
+                    int jCoord = j;
+                    int deep = L - 1;
+
+                    for (int dx = -deep; dx <= deep; dx++)
+                    {
+                        for (int dy = -deep; dy <= deep; dy++)
+                        {
+                            if (dx != 0 || dy != 0)
+                            {
+                                int newI = iCoord + dx;
+                                int newJ = jCoord + dy;
+
+                                if(IsPositionValid(table,newI,newJ))
+                                {
+                                    if(table[newI][newJ] != 'C')
+                                        table[newI][newJ] = '_';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)            
+                if(table[i][j] == 'X')
+                    sum += 1;
+        }
+        
+        Console.WriteLine(sum);
+    }
+
+    public static bool IsPositionValid(char[][] matrix, int rows, int columns)
+    {
+        int numRows = matrix.Length;
+        int numCols = matrix[0].Length;
+        bool state = (columns >= 0 && columns < numCols) && (rows >= 0 && rows < numRows);
+        return state;
+    }
+}
