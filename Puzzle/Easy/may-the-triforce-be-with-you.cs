5fa310a0fb9b490580ec9b0a516cd3c077561af1
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-12-02 18:17:55 -0500

    Add some code refactoring

--- Puzzle/Easy/may-the-triforce-be-with-you.cs
+++ Puzzle/Easy/may-the-triforce-be-with-you.cs
@@ -1,38 +1,32 @@
 using System;
 
-class Solution
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+int minf = 0;
+int countj = 0;
+int county = 1;
+int found = N*2-1;
+
+for(int i = 0; i <= found; i++)
 {
-    static void Main(string[] args)
+    if(i < N)
     {
-        int N = int.Parse(Console.ReadLine());
-        int count = 1;
-        int minf = 0;
-        int countj = 0;
-        int county = 1;
-        int found = N*2-1;
-
-        for(int i = 0; i <= found; i++)
+        if(i == 0)
         {
-            if(i < N)
-            {
-                if(i == 0)
-                {
-                    Console.WriteLine($".{new string(' ',found-i-1)}{new string('*',i+count)}");
-                }
-                else
-                {
-                    Console.WriteLine($"{new string(' ',found-i)}{new string('*',i+count)}");
-                }
-                count += 1;
-            }
-            else
-            {
-
-                Console.WriteLine($"{new string(' ',N-county)}{new string('*',countj+county)}{new string(' ',found-minf)}{new string('*',countj+county)}");
-                county += 1;
-                countj += 1;
-                minf += 2;
-            }
+            Console.WriteLine($".{new string(' ',found-i-1)}{new string('*',i+count)}");
         }
+        else
+        {
+            Console.WriteLine($"{new string(' ',found-i)}{new string('*',i+count)}");
+        }
+        count += 1;
+    }
+    else
+    {
+
+        Console.WriteLine($"{new string(' ',N-county)}{new string('*',countj+county)}{new string(' ',found-minf)}{new string('*',countj+county)}");
+        county += 1;
+        countj += 1;
+        minf += 2;
     }
 }
