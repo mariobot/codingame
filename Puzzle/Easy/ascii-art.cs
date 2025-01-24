c788fbfcef4c0f2c71e30240a4269c8a50b08c2f
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-01-24 17:15:31 -0500

    code changes

--- Puzzle/Easy/ascii-art.cs
+++ Puzzle/Easy/ascii-art.cs
@@ -2,73 +2,66 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+int L = int.Parse(Console.ReadLine());
+int H = int.Parse(Console.ReadLine());
+string T = Console.ReadLine();
+char[] word = T.ToLower().ToCharArray();
+char[] dic = "abcdefghijklmnopqrstuvwxyz?".ToCharArray();
+List<CharInfo> cInfo = new List<CharInfo>();
+for (int i = 0; i < H; i++)
 {
-    static void Main(string[] args)
+    string ROW = Console.ReadLine();
+
+    int index = 0;
+
+    if(i == 0)
     {
-        int L = int.Parse(Console.ReadLine());
-        int H = int.Parse(Console.ReadLine());
-        string T = Console.ReadLine();
-        char[] word = T.ToLower().ToCharArray();
-        char[] dic = "abcdefghijklmnopqrstuvwxyz?".ToCharArray();
-        List<CharInfo> cInfo = new List<CharInfo>();
-        for (int i = 0; i < H; i++)
+        foreach(char c in dic)
         {
-            string ROW = Console.ReadLine();
-
-            int index = 0;
-
-            if(i == 0)
-            {
-                foreach(char c in dic)
-                {
-                    string extracted = ROW.Substring(index, L);
-                    var mm = new CharInfo() 
-                    { 
-                        OriginalChar = c, Lines = new List<string>(),
-                    };
-                    mm.Lines.Add(extracted);
-                    cInfo.Add(mm);
-                    index += L;
-                }
-            }
-            else
-            {
-                foreach(char c in dic)
-                {
-                    string extracted = ROW.Substring(index, L);                    
-                    var mm = cInfo.Where(x => x.OriginalChar == c).FirstOrDefault();
-                    mm.Lines.Add(extracted);
-                    index += L;
-                }
-            }
+            string extracted = ROW.Substring(index, L);
+            var mm = new CharInfo() 
+            { 
+                OriginalChar = c, Lines = new List<string>(),
+            };
+            mm.Lines.Add(extracted);
+            cInfo.Add(mm);
+            index += L;
         }
-
-        for(int i = 0; i < H ; i++)
+    }
+    else
+    {
+        foreach(char c in dic)
         {
-            string row = "";
-            foreach(char w in word)
-            {
-                if(dic.Contains(w))
-                {
-                    var info = cInfo.Where(x => x.OriginalChar == w).FirstOrDefault();
-                    row += info.Lines[i];
-                }
-                else
-                {
-                    var info = cInfo.Where(x => x.OriginalChar == '?').FirstOrDefault();
-                    row += info.Lines[i];
-                }
-            }
-
-            Console.WriteLine(row);
+            string extracted = ROW.Substring(index, L);                    
+            var mm = cInfo.Where(x => x.OriginalChar == c).FirstOrDefault();
+            mm.Lines.Add(extracted);
+            index += L;
         }
-        
+    }
+}
+
+for(int i = 0; i < H ; i++)
+{
+    string row = "";
+    foreach(char w in word)
+    {
+        if(dic.Contains(w))
+        {
+            var info = cInfo.Where(x => x.OriginalChar == w).FirstOrDefault();
+            row += info.Lines[i];
+        }
+        else
+        {
+            var info = cInfo.Where(x => x.OriginalChar == '?').FirstOrDefault();
+            row += info.Lines[i];
+        }
     }
 
-    public class CharInfo
-    {
-        public char OriginalChar { get; set; }
-        public List<string> Lines {get; set; } = new List<string>();
-    }
+    Console.WriteLine(row);
+}
+
+public class CharInfo
+{
+    public char OriginalChar { get; set; }
+    public List<string> Lines {get; set; } = new List<string>();
 }
