c2e508def13cd2ada8e2af97ef82dc36d15529b4
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-01-30 14:13:36 -0500

    code changes

--- Puzzle/Easy/benfords-law.cs
+++ Puzzle/Easy/benfords-law.cs
@@ -2,44 +2,39 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+
+int N = int.Parse(Console.ReadLine());
+var list = new List<(string transaction, int value)>();
+
+for (int i = 0; i < N; i++)
 {
-    static void Main(string[] args)
+    string transaction = Console.ReadLine();
+    int value = transaction.FirstOrDefault(c => c != '0' && char.IsDigit(c)) - '0';
+    if (value > 0)
     {
-        int N = int.Parse(Console.ReadLine());
-        var list = new List<(string transaction, int value)>();
-
-        for (int i = 0; i < N; i++)
-        {
-            string transaction = Console.ReadLine();
-            int value = transaction.FirstOrDefault(c => c != '0' && char.IsDigit(c)) - '0';
-            if (value > 0)
-            {
-                list.Add((transaction, value));
-            }
-        }
-
-       var percentages = new Dictionary<int, double>
-        {
-            { 1, 30.1 },
-            { 2, 17.6 },
-            { 3, 12.5 },
-            { 4, 9.7 },
-            { 5, 7.9 },
-            { 6, 6.7 },
-            { 7, 5.8 },
-            { 8, 5.1 },
-            { 9, 4.6 }
-        };      
-        
-        double total = list.Count;
-        int state = percentages.Keys.Count(i =>
-        {
-            double countV = list.Count(x => x.value == i);
-            double p = countV / total * 100;
-            return p < percentages[i] - 10 || p > percentages[i] + 10;
-        });
-
-        Console.WriteLine(state > 0 ? "true" : "false");
+        list.Add((transaction, value));
     }
 }
+
+var percentages = new Dictionary<int, double>
+{
+    { 1, 30.1 },
+    { 2, 17.6 },
+    { 3, 12.5 },
+    { 4, 9.7 },
+    { 5, 7.9 },
+    { 6, 6.7 },
+    { 7, 5.8 },
+    { 8, 5.1 },
+    { 9, 4.6 }
+};      
+
+double total = list.Count;
+int state = percentages.Keys.Count(i =>
+{
+    double countV = list.Count(x => x.value == i);
+    double p = countV / total * 100;
+    return p < percentages[i] - 10 || p > percentages[i] + 10;
+});
+
+Console.WriteLine(state > 0 ? "true" : "false");
