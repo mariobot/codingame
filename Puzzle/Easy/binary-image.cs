f9b5ea22bd6b4182fe3b2e5ff3aeb2992975cb68
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-01-31 14:44:02 -0500

    code changes

--- Puzzle/Easy/binary-image.cs
+++ Puzzle/Easy/binary-image.cs
@@ -2,62 +2,57 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+
+int h = int.Parse(Console.ReadLine());
+List<(int[], bool)> list = new List<(int[], bool)>();
+for (int i = 0; i < h; i++)
 {
-    static void Main(string[] args)
+    string row = Console.ReadLine();
+    int[] arrayRow;
+    bool startZero = false;
+    if(row.StartsWith("0"))
     {
-        int h = int.Parse(Console.ReadLine());
-        List<(int[], bool)> list = new List<(int[], bool)>();
-        for (int i = 0; i < h; i++)
+        row = row.Remove(0, 2);
+        arrayRow = row.Split(' ').Select(x => int.Parse(x)).ToArray();
+        startZero = true;
+    }
+    else
+    {
+        arrayRow = row.Split(' ').Select(x => int.Parse(x)).ToArray();
+    }
+    list.Add(new (arrayRow, startZero));
+}
+
+char symbol = '.';
+int sumLine = list.FirstOrDefault().Item1.Sum();
+bool nolineal = false;
+
+for (int i = 0; i < h; i++)
+{
+    if(sumLine !=  list[i].Item1.Sum())
+        nolineal = true;
+}
+
+if(nolineal)
+    Console.WriteLine("INVALID");
+else
+{
+    for (int i = 0; i < h; i++)
+    {
+        var item = list[i];
+        string line = "";
+
+        if(item.Item2)            
+            symbol = 'O';            
+        else            
+            symbol = '.';
+
+        for(int j = 0; j < item.Item1.Length; j++)
         {
-            string row = Console.ReadLine();
-            int[] arrayRow;
-            bool startZero = false;
-            if(row.StartsWith("0"))
-            {
-                row = row.Remove(0, 2);
-                arrayRow = row.Split(' ').Select(x => int.Parse(x)).ToArray();
-                startZero = true;
-            }
-            else
-            {
-                arrayRow = row.Split(' ').Select(x => int.Parse(x)).ToArray();
-            }
-            list.Add(new (arrayRow, startZero));
+            line += new string(symbol, item.Item1[j]);
+            symbol = symbol == 'O' ? '.' : 'O';                
         }
 
-        char symbol = '.';
-        int sumLine = list.FirstOrDefault().Item1.Sum();
-        bool nolineal = false;
-        
-        for (int i = 0; i < h; i++)
-        {
-            if(sumLine !=  list[i].Item1.Sum())
-                nolineal = true;
-        }
-
-        if(nolineal)
-            Console.WriteLine("INVALID");
-        else
-        {
-            for (int i = 0; i < h; i++)
-            {
-                var item = list[i];
-                string line = "";
-
-                if(item.Item2)            
-                    symbol = 'O';            
-                else            
-                    symbol = '.';
-
-                for(int j = 0; j < item.Item1.Length; j++)
-                {
-                    line += new string(symbol, item.Item1[j]);
-                    symbol = symbol == 'O' ? '.' : 'O';                
-                }
-
-                Console.WriteLine(line);
-            }
-        }
+        Console.WriteLine(line);
     }
 }
