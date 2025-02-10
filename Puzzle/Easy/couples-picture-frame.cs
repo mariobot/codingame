bbb6868eb817c9fa3efb03373b05cdd280d17d3d
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-10 18:37:57 -0500

    code changes

--- Puzzle/Easy/couples-picture-frame.cs
+++ Puzzle/Easy/couples-picture-frame.cs
@@ -1,56 +1,51 @@
 using System;
 using System.Linq;
 
-class Solution
+string wife = Console.ReadLine();
+string husband = Console.ReadLine();
+int w = LCM(wife.Length, husband.Length);
+
+Console.WriteLine(string.Join("", Enumerable.Repeat(wife, w/wife.Length)));
+PrintBody(wife, husband, w);
+Console.WriteLine(string.Join("", Enumerable.Repeat(husband, w/husband.Length)));
+
+
+public static void PrintBody(string wife, string husband, int w)
 {
-    static void Main(string[] args)
+    for(int i = 0; i < w; i++)
     {
-        string wife = Console.ReadLine();
-        string husband = Console.ReadLine();
-        int w = LCM(wife.Length, husband.Length);
+        int w1 = i;
+        int h1 = i;
+        if(i >= wife.Length)
+            w1 = i % wife.Length;
+        if(i >= husband.Length)
+            h1 = i % husband.Length;
 
-        Console.WriteLine(string.Join("", Enumerable.Repeat(wife, w/wife.Length)));
-        PrintBody(wife, husband, w);
-        Console.WriteLine(string.Join("", Enumerable.Repeat(husband, w/husband.Length)));
+        Console.WriteLine(husband[h1]+new string(' ', w-2)+wife[w1]);
+    }
+}
+
+public static int LCM(int a, int b)
+{
+    int num1, num2;
+
+    if (a > b)
+    {
+        num1 = a;
+        num2 = b;
+    }
+    else
+    {
+        num1 = b;
+        num2 = a;
     }
 
-    public static void PrintBody(string wife, string husband, int w)
+    for (int i = 1; i <= num2; i++)
     {
-        for(int i = 0; i < w; i++)
+        if ((num1 * i) % num2 == 0)
         {
-            int w1 = i;
-            int h1 = i;
-            if(i >= wife.Length)
-                w1 = i % wife.Length;
-            if(i >= husband.Length)
-                h1 = i % husband.Length;
-
-            Console.WriteLine(husband[h1]+new string(' ', w-2)+wife[w1]);
+            return i * num1;
         }
     }
-
-    public static int LCM(int a, int b)
-    {
-        int num1, num2;
-
-        if (a > b)
-        {
-            num1 = a;
-            num2 = b;
-        }
-        else
-        {
-            num1 = b;
-            num2 = a;
-        }
-
-        for (int i = 1; i <= num2; i++)
-        {
-            if ((num1 * i) % num2 == 0)
-            {
-                return i * num1;
-            }
-        }
-        return num2;
-    }
+    return num2;
 }
