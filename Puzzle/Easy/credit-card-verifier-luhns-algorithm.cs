2102ede5ca693bfce56a23b8f556ec7b97cc4c03
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-14 17:33:23 -0500

    code change

--- Puzzle/Easy/credit-card-verifier-luhns-algorithm.cs
+++ Puzzle/Easy/credit-card-verifier-luhns-algorithm.cs
@@ -1,42 +1,36 @@
 using System;
 using System.Linq;
 
-class Solution
+int n = int.Parse(Console.ReadLine());
+int[] first = new int[8];
+
+for (int i = 0; i < n; i++)
 {
-    static void Main(string[] args)
+    string card = Console.ReadLine().Replace(" ","");
+    int second = 0;
+    int inc = 0;
+
+    for(int j = card.Length-1; j >= 0; j--)
     {
-        int n = int.Parse(Console.ReadLine());
-        int[] first = new int[8];
-        
-        for (int i = 0; i < n; i++)
+        if(j % 2 == 0)
         {
-            string card = Console.ReadLine().Replace(" ","");
-            int second = 0;
-            int inc = 0;
-
-            for(int j = card.Length-1; j >= 0; j--)
-            {
-                if(j % 2 == 0)
-                {
-                    int f = int.Parse(card[j].ToString())*2;
-                    
-                    if(f >= 10)
-                        first[inc] = int.Parse(f.ToString()[0].ToString()) + int.Parse(f.ToString()[1].ToString());
-                    else
-                        first[inc] = f;
-
-                    inc += 1;
-                }
-                else                
-                    second += int.Parse(card[j].ToString());
-            }
-
-            int total = first.Sum() + second;
-
-            if(total % 10 == 0)
-                Console.WriteLine("YES");
+            int f = int.Parse(card[j].ToString())*2;
+            
+            if(f >= 10)
+                first[inc] = int.Parse(f.ToString()[0].ToString()) + int.Parse(f.ToString()[1].ToString());
             else
-                Console.WriteLine("NO");
+                first[inc] = f;
+
+            inc += 1;
         }
+        else                
+            second += int.Parse(card[j].ToString());
     }
+
+    int total = first.Sum() + second;
+
+    if(total % 10 == 0)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
 }
