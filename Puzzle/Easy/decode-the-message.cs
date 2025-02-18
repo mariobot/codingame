cd56c46d98c9b4a8cd805628bcaabd7cb0abd13e
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-18 16:00:46 -0500

    code changes

--- Puzzle/Easy/decode-the-message.cs
+++ Puzzle/Easy/decode-the-message.cs
@@ -1,38 +1,32 @@
 using System;
 using System.Collections.Generic;
 
-class Solution
+long P = long.Parse(Console.ReadLine());
+int length = 0;
+long Pt = P;
+string C = Console.ReadLine();
+List<char> list = new List<char>();
+bool isFirst = true;
+length = C.Length;
+
+while (Pt > 0)
 {
-    static void Main(string[] args)
+    long remainder = Pt % length;
+    if(isFirst)
     {
-        long P = long.Parse(Console.ReadLine());
-        int length = 0;
-        long Pt = P;
-        string C = Console.ReadLine();
-        List<char> list = new List<char>();
-        bool isFirst = true;
-        length = C.Length;
-
-        while (Pt > 0)
-        {
-            long remainder = Pt % length;
-            if(isFirst)
-            {
-                list.Add(C[(int)remainder]);
-                isFirst = false;
-            }
-            else
-            {
-                if (((int)remainder-1) == -1){
-                    Pt -= length;
-                }   
-                else
-                    list.Add(C[(int)remainder-1]);
-            }
-
-            Pt /= length;
-        }
-
-        Console.WriteLine(string.Join("",list));
+        list.Add(C[(int)remainder]);
+        isFirst = false;
     }
+    else
+    {
+        if (((int)remainder-1) == -1){
+            Pt -= length;
+        }   
+        else
+            list.Add(C[(int)remainder-1]);
+    }
+
+    Pt /= length;
 }
+
+Console.WriteLine(string.Join("",list));
