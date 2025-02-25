0888390ab8bfea43fcc9711c6c6b9dd49c57392a
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-25 18:21:34 -0500

    code changes

--- Puzzle/Easy/encryptiondecryption-of-enigma-machine.cs
+++ Puzzle/Easy/encryptiondecryption-of-enigma-machine.cs
@@ -2,124 +2,120 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+public const string dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+string operation = Console.ReadLine();
+int RandomNumber = int.Parse(Console.ReadLine());
+List<string> rotors = new List<string>();
+
+for (int i = 0; i < 3; i++)
 {
-    public const string dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    static void Main(string[] args)
+    string rotor = Console.ReadLine();
+    rotors.Add(rotor);
+}
+
+string message = Console.ReadLine();
+string R = "";
+
+if(operation == "ENCODE")
+{
+    R = First(message, RandomNumber);
+    R = EncodeRotor(R, rotors[0]);
+    R = EncodeRotor(R, rotors[1]);
+    R = EncodeRotor(R, rotors[2]);
+}
+else
+{
+    R = DecodeRotor(message, rotors[2]);
+    R = DecodeRotor(R, rotors[1]);
+    R = DecodeRotor(R, rotors[0]);
+    R = DecodeLast(R, RandomNumber);
+}
+
+Console.WriteLine(R);
+    
+
+static string First(string input, int RandomNumber)
+{
+    string result = "";
+
+    for(int i = 0; i < input.Length; i++)
     {
-        string operation = Console.ReadLine();
-        int RandomNumber = int.Parse(Console.ReadLine());
-        List<string> rotors = new List<string>();
-
-        for (int i = 0; i < 3; i++)
-        {
-            string rotor = Console.ReadLine();
-            rotors.Add(rotor);
-        }
-        
-        string message = Console.ReadLine();
-        string R = "";
-
-        if(operation == "ENCODE")
-        {
-            R = First(message, RandomNumber);
-            R = EncodeRotor(R, rotors[0]);
-            R = EncodeRotor(R, rotors[1]);
-            R = EncodeRotor(R, rotors[2]);
-        }
-        else
-        {
-            R = DecodeRotor(message, rotors[2]);
-            R = DecodeRotor(R, rotors[1]);
-            R = DecodeRotor(R, rotors[0]);
-            R = DecodeLast(R, RandomNumber);
-        }
-
-        Console.WriteLine(R);
+        int index = dic.IndexOf(input[i]) + RandomNumber + i;
+        if(index >= 26)
+            index = IndPoss(index);
+        result += dic.Substring(index,1);
     }
 
-    static string First(string input, int RandomNumber)
+    return result;
+}
+
+static string EncodeRotor(string input, string rotor)
+{
+    string result = "";
+
+    for(int i = 0; i < input.Length; i++)
     {
-        string result = "";
-
-        for(int i = 0; i < input.Length; i++)
-        {
-            int index = dic.IndexOf(input[i]) + RandomNumber + i;
-            if(index >= 26)
-                index = IndPoss(index);
-            result += dic.Substring(index,1);
-        }
-
-        return result;
+        int index = dic.IndexOf(input[i]);
+        result += rotor.Substring(index,1);
     }
 
-    static string EncodeRotor(string input, string rotor)
+    return result;
+}
+
+static string DecodeRotor(string input, string rotor)
+{
+    string result = "";
+
+    for(int i = 0; i < input.Length; i++)
     {
-        string result = "";
-
-        for(int i = 0; i < input.Length; i++)
-        {
-            int index = dic.IndexOf(input[i]);
-            result += rotor.Substring(index,1);
-        }
-
-        return result;
+        int index = rotor.IndexOf(input[i]);
+        result += dic.Substring(index,1);
     }
 
-    static string DecodeRotor(string input, string rotor)
+    return result;
+}
+
+static string DecodeLast(string input, int RandomNumber)
+{
+    string result = "";
+
+    for(int i = 0; i < input.Length; i++)
     {
-        string result = "";
+        int index = dic.IndexOf(input[i]) - RandomNumber - i;
+        if(index < 0)            
+            index = IndNeg(index);
+        if(index >= 26)
+            index = IndPoss(index);
 
-        for(int i = 0; i < input.Length; i++)
-        {
-            int index = rotor.IndexOf(input[i]);
-            result += dic.Substring(index,1);
-        }
-
-        return result;
+        result += dic.Substring(index,1);
     }
 
-    static string DecodeLast(string input, int RandomNumber)
+    return result;
+}
+
+static int IndPoss(int val)
+{
+    if(val >= 26)
     {
-        string result = "";
+        val = val - 26;
 
-        for(int i = 0; i < input.Length; i++)
-        {
-            int index = dic.IndexOf(input[i]) - RandomNumber - i;
-            if(index < 0)            
-                index = IndNeg(index);
-            if(index >= 26)
-                index = IndPoss(index);
-
-            result += dic.Substring(index,1);
-        }
-
-        return result;
-    }
-
-    static int IndPoss(int val)
-    {
         if(val >= 26)
-        {
-            val = val - 26;
-
-            if(val >= 26)
-             val = IndPoss(val);
-        }
-
-        return val;
+            val = IndPoss(val);
     }
 
-    static int IndNeg(int val)
+    return val;
+}
+
+static int IndNeg(int val)
+{
+    if(val < 0)
     {
+        val = 26 + val;
+
         if(val < 0)
-        {
-            val = 26 + val;
-
-            if(val < 0)
-                val = IndNeg(val);
-        }
-
-        return val;
+            val = IndNeg(val);
     }
+
+    return val;
 }
