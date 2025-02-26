cf7dbebb6aa0af43363373b514f3394880ea6d26
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-26 18:03:08 -0500

    code changes

--- Puzzle/Easy/fax-machine.cs
+++ Puzzle/Easy/fax-machine.cs
@@ -2,53 +2,49 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+
+int W = int.Parse(Console.ReadLine());
+int H = int.Parse(Console.ReadLine());        
+List<int> values = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
+
+int Total = W*H;
+string result = "|";
+char symbol = '*';
+int changeIn = values[0];
+
+for(int i = 1; i < Total+1 ; i ++)
 {
-    static void Main(string[] args)
+    if(i == changeIn + 1 && changeIn <= Total)
     {
-        int W = int.Parse(Console.ReadLine());
-        int H = int.Parse(Console.ReadLine());        
-        List<int> values = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
-        
-        int Total = W*H;
-        string result = "|";
-        char symbol = '*';
-        int changeIn = values[0];
+        symbol = symbol == '*' ? ' ' : '*';
 
-        for(int i = 1; i < Total+1 ; i ++)
-        {
-            if(i == changeIn + 1 && changeIn <= Total)
-            {
-                symbol = symbol == '*' ? ' ' : '*';
+        values = NextValue(values);
 
-                values = NextValue(values);
-
-                if(values != null)                
-                    changeIn = i + values[0] -1;
-            }
-
-            if(i != 1)
-            {
-                if(i % W == 1)
-                    result += "\n|";
-
-                result += symbol;
-
-                if(i % W == 0)
-                    result += "|";
-            }
-            else
-            {
-                result += symbol;
-            }
-        }
-
-        Console.WriteLine(result);
+        if(values != null)                
+            changeIn = i + values[0] -1;
     }
 
-    public static List<int> NextValue(List<int> values)
+    if(i != 1)
     {
-        values.Remove(values.First());
-        return values;
+        if(i % W == 1)
+            result += "\n|";
+
+        result += symbol;
+
+        if(i % W == 0)
+            result += "|";
     }
+    else
+    {
+        result += symbol;
+    }
+}
+
+Console.WriteLine(result);
+
+
+public static List<int> NextValue(List<int> values)
+{
+    values.Remove(values.First());
+    return values;
 }
