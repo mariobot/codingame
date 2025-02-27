b1f16283d25d0fd9bd753617b6a3c4615aca6010
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-02-27 15:03:56 -0500

    code changes

--- Puzzle/Easy/fix-the-networks.cs
+++ Puzzle/Easy/fix-the-networks.cs
@@ -1,45 +1,40 @@
 using System;
 using System.Linq;
 
-class Solution
+
+int m = int.Parse(Console.ReadLine());
+for (int i = 0; i < m; i++)
 {
-    static void Main(string[] args)
+    string[] r = Console.ReadLine().Split("/");
+    string[] ip = r[0].Split(".");
+    int mask = int.Parse(r[1]);
+    int totalZeros, totalMask;
+
+    string binnaryIp = "";
+    
+    foreach(string val in ip)
+        binnaryIp += Convert.ToString(int.Parse(val), 2).PadLeft(8, '0');
+
+    var lastIndex = binnaryIp.LastIndexOf('1');
+
+    totalZeros = 31 - lastIndex;
+    totalMask = 32 - mask;
+
+    if(binnaryIp.Count(x => x =='0') == 32)
     {
-        int m = int.Parse(Console.ReadLine());
-        for (int i = 0; i < m; i++)
-        {
-            string[] r = Console.ReadLine().Split("/");
-            string[] ip = r[0].Split(".");
-            int mask = int.Parse(r[1]);
-            int totalZeros, totalMask;
+        totalZeros = 32;
+        totalMask = 32 - mask;
+    }
 
-            string binnaryIp = "";
-            
-            foreach(string val in ip)
-                binnaryIp += Convert.ToString(int.Parse(val), 2).PadLeft(8, '0');
-
-            var lastIndex = binnaryIp.LastIndexOf('1');
-
-            totalZeros = 31 - lastIndex;
-            totalMask = 32 - mask;
-
-            if(binnaryIp.Count(x => x =='0') == 32)
-            {
-                totalZeros = 32;
-                totalMask = 32 - mask;
-            }
-
-            if(totalZeros >= totalMask)
-            {
-                long totalConvinations = (long)Math.Pow((double)2, (double)totalMask);
-                Console.WriteLine($"valid {totalConvinations}");
-            }
-            else
-            {
-                int newMask = 32 - totalZeros;
-                long totalConvinations = (long)Math.Pow((double)2, (double)totalZeros);
-                Console.WriteLine($"invalid {string.Join(".",ip)}/{newMask} {totalConvinations}");
-            }
-        }
+    if(totalZeros >= totalMask)
+    {
+        long totalConvinations = (long)Math.Pow((double)2, (double)totalMask);
+        Console.WriteLine($"valid {totalConvinations}");
+    }
+    else
+    {
+        int newMask = 32 - totalZeros;
+        long totalConvinations = (long)Math.Pow((double)2, (double)totalZeros);
+        Console.WriteLine($"invalid {string.Join(".",ip)}/{newMask} {totalConvinations}");
     }
 }
