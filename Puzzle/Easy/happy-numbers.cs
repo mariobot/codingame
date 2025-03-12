b70b6dd41455f4f857c42c456d2b3475d7596745
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-03-12 17:01:25 -0500

    code changes

--- Puzzle/Easy/happy-numbers.cs
+++ Puzzle/Easy/happy-numbers.cs
@@ -2,45 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 
-class Solution
+
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
 {
-    static void Main(string[] args)
+    string x = Console.ReadLine();
+    if(IsHappy(BigInteger.Parse(x)))
+        Console.WriteLine($"{x} :)");
+    else
+        Console.WriteLine($"{x} :(");
+}
+    
+static bool IsHappy(BigInteger number)
+{
+    HashSet<BigInteger> seenNumbers = new HashSet<BigInteger>();
+
+    while (number != 1 && !seenNumbers.Contains(number))
     {
-        int N = int.Parse(Console.ReadLine());
-        for (int i = 0; i < N; i++)
-        {
-            string x = Console.ReadLine();
-            if(IsHappy(BigInteger.Parse(x)))
-                Console.WriteLine($"{x} :)");
-            else
-                Console.WriteLine($"{x} :(");
-        }
+        seenNumbers.Add(number);
+        number = SumOfSquaresOfDigits(number);
     }
 
-    static bool IsHappy(BigInteger number)
+    return number == 1;
+}
+
+static BigInteger SumOfSquaresOfDigits(BigInteger number)
+{
+    BigInteger sum = 0;
+
+    while (number > 0)
     {
-        HashSet<BigInteger> seenNumbers = new HashSet<BigInteger>();
-
-        while (number != 1 && !seenNumbers.Contains(number))
-        {
-            seenNumbers.Add(number);
-            number = SumOfSquaresOfDigits(number);
-        }
-
-        return number == 1;
+        BigInteger digit = number % 10;
+        sum += digit * digit;
+        number /= 10;
     }
 
-    static BigInteger SumOfSquaresOfDigits(BigInteger number)
-    {
-        BigInteger sum = 0;
-
-        while (number > 0)
-        {
-            BigInteger digit = number % 10;
-            sum += digit * digit;
-            number /= 10;
-        }
-
-        return sum;
-    }
+    return sum;
 }
