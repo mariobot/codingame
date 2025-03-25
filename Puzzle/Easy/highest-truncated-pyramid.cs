08d4916d6a70c11d5458bbeaa804fd0b6a3e813a
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-03-25 15:49:17 -0500

    code changes

--- Puzzle/Easy/highest-truncated-pyramid.cs
+++ Puzzle/Easy/highest-truncated-pyramid.cs
@@ -1,44 +1,39 @@
 using System;
 
-class Solution
+
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int startLev = ReturnLevel(N);
+
+for(int i = startLev; i <= N; i++)
 {
-    static void Main(string[] args)
+    Console.WriteLine(new string('*',i));
+    sum += i;
+    if(sum == N)
+        break;
+}
+ 
+public static int CheckLevel(int N, int level)
+{
+    int sum = 0;
+    for(int i = level; i <= N*10; i++)
     {
-        int N = int.Parse(Console.ReadLine());
-        int sum = 0;
+        sum += i;
+        if(sum == N)            
+            return level;            
+    }
+    return 0;
+}
 
-        int startLev = ReturnLevel(N);
-        
-        for(int i = startLev; i <= N; i++)
-        {
-            Console.WriteLine(new string('*',i));
-            sum += i;
-            if(sum == N)
-                break;
-        }
+public static int ReturnLevel(int N)
+{
+    for(int i = 0; i <= 8; i++)
+    {
+        int lev = CheckLevel(N, i);
+        if( lev != 0)
+            return lev;
     }
 
-    public static int CheckLevel(int N, int level)
-    {
-        int sum = 0;
-        for(int i = level; i <= N*10; i++)
-        {
-            sum += i;
-            if(sum == N)            
-                return level;            
-        }
-        return 0;
-    }
-
-    public static int ReturnLevel(int N)
-    {
-        for(int i = 0; i <= 8; i++)
-        {
-            int lev = CheckLevel(N, i);
-            if( lev != 0)
-                return lev;
-        }
-
-        return 0;
-    }
+    return 0;
 }
