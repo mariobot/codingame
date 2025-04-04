1fc0f015c80f6591187d8d316110f64a257c0821
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-04-04 17:31:50 -0500

    code changes

--- Puzzle/Easy/mime-type.cs
+++ Puzzle/Easy/mime-type.cs
@@ -2,38 +2,33 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+
+int N = int.Parse(Console.ReadLine());
+int Q = int.Parse(Console.ReadLine());
+Dictionary<string, string> mimeTypes = new Dictionary<string, string>();
+
+for (int i = 0; i < N; i++)
 {
-    static void Main(string[] args)
+    string[] inputs = Console.ReadLine().Split(' ');
+    string extension = inputs[0].ToLower();
+    string mimeType = inputs[1];
+    if (!mimeTypes.ContainsKey(extension))
     {
-        int N = int.Parse(Console.ReadLine());
-        int Q = int.Parse(Console.ReadLine());
-        Dictionary<string, string> mimeTypes = new Dictionary<string, string>();
+        mimeTypes.Add(extension, mimeType);
+    }
+}
 
-        for (int i = 0; i < N; i++)
-        {
-            string[] inputs = Console.ReadLine().Split(' ');
-            string extension = inputs[0].ToLower();
-            string mimeType = inputs[1];
-            if (!mimeTypes.ContainsKey(extension))
-            {
-                mimeTypes.Add(extension, mimeType);
-            }
-        }
-
-        for (int i = 0; i < Q; i++)
-        {
-            string fileName = Console.ReadLine();
-            string[] fileParts = fileName.Split('.');
-            string extension = fileParts.Length >= 2 ? fileParts.Last().ToLower() : "empty";
-            if (mimeTypes.TryGetValue(extension, out string mimeType))
-            {
-                Console.WriteLine(mimeType);
-            }
-            else
-            {
-                Console.WriteLine("UNKNOWN");
-            }
-        }
+for (int i = 0; i < Q; i++)
+{
+    string fileName = Console.ReadLine();
+    string[] fileParts = fileName.Split('.');
+    string extension = fileParts.Length >= 2 ? fileParts.Last().ToLower() : "empty";
+    if (mimeTypes.TryGetValue(extension, out string mimeType))
+    {
+        Console.WriteLine(mimeType);
+    }
+    else
+    {
+        Console.WriteLine("UNKNOWN");
     }
 }
