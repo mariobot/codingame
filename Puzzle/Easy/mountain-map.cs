37bd9ab5fbd6dd629ecf36c69a130f9eae9e4c82
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-04-07 15:54:15 -0500

    code changes

--- Puzzle/Easy/mountain-map.cs
+++ Puzzle/Easy/mountain-map.cs
@@ -1,72 +1,66 @@
 using System;
 using System.Linq;
 
-class Mountains
+// Read the number of mountains
+int n = int.Parse(Console.ReadLine().Trim());
+
+// Read the heights of the mountains
+string[] inputHeights = Console.ReadLine().Trim().Split(' ');
+int[] heights = Array.ConvertAll(inputHeights, int.Parse);
+var (maxH, index) = heights.Select((n, i) => (n, i)).Max();
+
+PrintMountains(n, heights, maxH);
+    
+static void PrintMountains(int n, int[] heights, int maxH)
 {
-    static void Main(string[] args)
+    int maxHeight = maxH;
+    
+    int width = 0;
+    foreach (int height in heights)        
+        width += 2 * height;                    
+
+    char[,] grid = new char[maxHeight, width];
+    
+    // Initialize grid with spaces
+    for (int i = 0; i < maxHeight; i++)
     {
-        // Read the number of mountains
-        int n = int.Parse(Console.ReadLine().Trim());
-        
-        // Read the heights of the mountains
-        string[] inputHeights = Console.ReadLine().Trim().Split(' ');
-        int[] heights = Array.ConvertAll(inputHeights, int.Parse);
-        var (maxH, index) = heights.Select((n, i) => (n, i)).Max();
-        
-        PrintMountains(n, heights, maxH);
+        for (int j = 0; j < width; j++)            
+            grid[i, j] = ' ';
     }
     
-    static void PrintMountains(int n, int[] heights, int maxH)
+    int currentPos = 0;
+
+    for (int i = 0; i < n; i++)
     {
-        int maxHeight = maxH;
-        
-        int width = 0;
-        foreach (int height in heights)        
-            width += 2 * height;                    
+        int height = heights[i];
 
-        char[,] grid = new char[maxHeight, width];
-        
-        // Initialize grid with spaces
-        for (int i = 0; i < maxHeight; i++)
+        // Draw the rising part of the mountain
+        for (int j = 0; j < height; j++)
         {
-            for (int j = 0; j < width; j++)            
-                grid[i, j] = ' ';
+            grid[maxHeight - 1 - j, currentPos + j] = '/';
         }
-        
-        int currentPos = 0;
 
-        for (int i = 0; i < n; i++)
+        currentPos += height;
+
+        // Draw the falling part of the mountain
+        for (int j = 0; j < height; j++)
         {
-            int height = heights[i];
-
-            // Draw the rising part of the mountain
-            for (int j = 0; j < height; j++)
-            {
-                grid[maxHeight - 1 - j, currentPos + j] = '/';
-            }
-
-            currentPos += height;
-
-            // Draw the falling part of the mountain
-            for (int j = 0; j < height; j++)
-            {
-                grid[maxHeight - height + j, currentPos + j] = '\\';
-            }
-
-            currentPos += height;
+            grid[maxHeight - height + j, currentPos + j] = '\\';
         }
-        
-        // Print the grid without trailing spaces
-        string line = "";
-        
-        for (int i = 0; i < maxHeight; i++)
+
+        currentPos += height;
+    }
+    
+    // Print the grid without trailing spaces
+    string line = "";
+    
+    for (int i = 0; i < maxHeight; i++)
+    {
+        line = "";
+        for (int j = 0; j < width; j++)
         {
-            line = "";
-            for (int j = 0; j < width; j++)
-            {
-                line += grid[i, j];
-            }
-            Console.WriteLine(line.TrimEnd());
+            line += grid[i, j];
         }
+        Console.WriteLine(line.TrimEnd());
     }
 }
