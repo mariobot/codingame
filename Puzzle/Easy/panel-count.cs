27bfec00953e6d93a233e05c95ce4e5320fb5dfa
Mario Andres Botero Florez <mariobot113@hotmail.com>
2025-04-16 19:30:52 -0500

    code changes

--- Puzzle/Easy/panel-count.cs
+++ Puzzle/Easy/panel-count.cs
@@ -2,78 +2,72 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class Solution
+int P = int.Parse(Console.ReadLine());
+List<string> filters = new List<string>();
+List<string[]> lines = new List<string[]>();
+List<List<Operation>> formulas = new List<List<Operation>>();
+
+for (int i = 0; i < P; i++)
 {
-    static void Main(string[] args)
+    string property = Console.ReadLine();
+    filters.Add(property);
+}
+
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    string[] l = Console.ReadLine().Split(" ");
+    lines.Add(l);
+}
+
+int F = int.Parse(Console.ReadLine());
+for (int i = 0; i < F; i++)
+{
+    string f = Console.ReadLine();
+    List<Operation> list = new List<Operation>();
+
+    if(f.Contains(" AND "))
     {
-        int P = int.Parse(Console.ReadLine());
-        List<string> filters = new List<string>();
-        List<string[]> lines = new List<string[]>();
-        List<List<Operation>> formulas = new List<List<Operation>>();
-
-        for (int i = 0; i < P; i++)
-        {
-            string property = Console.ReadLine();
-            filters.Add(property);
-        }
-        
-        int N = int.Parse(Console.ReadLine());
-        for (int i = 0; i < N; i++)
-        {
-            string[] l = Console.ReadLine().Split(" ");
-            lines.Add(l);
-        }
-        
-        int F = int.Parse(Console.ReadLine());
-        for (int i = 0; i < F; i++)
-        {
-            string f = Console.ReadLine();
-            List<Operation> list = new List<Operation>();
-
-            if(f.Contains(" AND "))
-            {
-                string[] sentence = f.Split(" AND ");
-                foreach(string w in sentence)                
-                    list.Add(FormatOperation(w));
-            }
-            else            
-                list.Add(FormatOperation(f));
-            
-            formulas.Add(list);            
-        }
-
-        foreach(List<Operation> Os in formulas)
-        {
-            var linesCopy = lines;
-            foreach(Operation o in Os)            
-                linesCopy = Filter(linesCopy, o.Value, filters.IndexOf(o.Filter)+1);                            
-            Console.WriteLine(linesCopy.Count());
-        }
+        string[] sentence = f.Split(" AND ");
+        foreach(string w in sentence)                
+            list.Add(FormatOperation(w));
     }
+    else            
+        list.Add(FormatOperation(f));
+    
+    formulas.Add(list);            
+}
 
-    public static List<string[]> Filter(List<string[]> list, string value, int i = 0)
-    {
-        List<string[]> result = list.Where(x => x[i] == value).ToList();
-        return result;
-    }
+foreach(List<Operation> Os in formulas)
+{
+    var linesCopy = lines;
+    foreach(Operation o in Os)            
+        linesCopy = Filter(linesCopy, o.Value, filters.IndexOf(o.Filter)+1);                            
+    Console.WriteLine(linesCopy.Count());
+}
 
-    public static Operation FormatOperation(string line)
-    {
-        int ind = line.IndexOf('=');
-        string filter = line.Substring(0,ind);
-        string value = line.Substring(ind+1,line.Length-(ind+1));
+public static List<string[]> Filter(List<string[]> list, string value, int i = 0)
+{
+    List<string[]> result = list.Where(x => x[i] == value).ToList();
+    return result;
+}
 
-        Operation op = new Operation(){
-            Filter = filter,
-            Value = value
-        };
+public static Operation FormatOperation(string line)
+{
+    int ind = line.IndexOf('=');
+    string filter = line.Substring(0,ind);
+    string value = line.Substring(ind+1,line.Length-(ind+1));
 
-        return op;
-    }
+    Operation op = new Operation(){
+        Filter = filter,
+        Value = value
+    };
 
-    public class Operation
-    {
-        public string Filter { get; set; }
-        public string Value { get; set; }
-    }
+    return op;
+}
+
+public class Operation
+{
+    public string Filter { get; set; }
+    public string Value { get; set; }
 }
